bed33988c956022f3efb55b1b7d2086e94f40054
Roby Morgan <roby.c.morgan@gmail.com>
2022-01-11 22:46:15 +0100

    fetch data.

--- src/components/blog/blog-list/index.jsx
+++ src/components/blog/blog-list/index.jsx
@@ -1,17 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import { Row, Col } from "react-bootstrap";
 import BlogItem from "../blog-item";
-import posts from "../../../data/posts.json";
-export default class BlogList extends Component {
-  render() {
-    return (
-      <Row>
-        {posts.map((post) => (
-          <Col md={4} style={{ marginBottom: 50 }}>
-            <BlogItem key={post.title} {...post} />
-          </Col>
-        ))}
-      </Row>
-    );
-  }
+// import posts from "../../../data/posts.json";
+
+
+const Bloglists = ({ posts }) => {
+  console.log(posts)
+  return (
+    <Row>
+      {posts.map((post) => (
+        <Col md={4} style={{ marginBottom: 50 }}>
+          <BlogItem key={post.title} {...post} />
+        </Col>
+      ))}
+    </Row>
+  );
 }
+export default Bloglists
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,15 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import { Container } from "react-bootstrap";
 import BlogList from "../../components/blog/blog-list";
 import "./styles.css";
+import { useEffect, useState } from "react";
 
-export default class Home extends Component {
-  render() {
-    return (
-      <Container fluid="sm">
-        <h1 className="blog-main-title">Welcome to the Strive Blog!</h1>
-        <BlogList />
-      </Container>
-    );
+const Home = () => {
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState([])
+  const fetchBlogs = async () => {
+    try {
+      const response = await fetch("http://localhost:3005/blogs")
+      if (response.ok) {
+        const result = await response.json()
+        setData(result)
+        setLoading(false)
+      } else { throw new Error("Fetching Failed!") }
+    } catch (error) {
+      console.log(error)
+    }
   }
+  useEffect(() => {
+    fetchBlogs()
+  }, [])
+  return (
+    <Container fluid="sm" >
+      <h1 className="blog-main-title">Welcome to the Strive Blog!</h1>
+      <BlogList posts={data} />
+    </Container>
+  );
 }
+
+export default Home
